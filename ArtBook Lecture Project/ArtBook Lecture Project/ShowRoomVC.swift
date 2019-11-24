be86177556ecb4eabc7e73e7da88b977626ab554
Murat Han Acıpayam <acipayammurathan@gmail.com>
2019-11-24 20:46:36 +0300

    Add files via upload

---
+++ ArtBook Lecture Project/ArtBook Lecture Project/ShowRoomVC.swift
@@ -0,0 +1,30 @@
+//
+//  ShowRoomVC.swift
+//  ArtBook Lecture Project
+//
+//  Created by Murat Han on 24.11.2019.
+//  Copyright © 2019 Murat Han. All rights reserved.
+//
+
+import UIKit
+
+class ShowRoomVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
